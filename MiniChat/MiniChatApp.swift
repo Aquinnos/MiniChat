48fb66f140a7b6d872bf7b66594d808bf1471291
Krystian Synakowski <k.synakowski123@gmail.com>
2026-06-05 21:18:12 +0200

    Initial Commit

---
+++ MiniChat/MiniChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChatApp.swift
+//  MiniChat
+//
+//  Created by Krystian Synakowski on 05/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MiniChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiniChatTests/MiniChatTests.swift
@@ -0,0 +1,18 @@
+//
+//  MiniChatTests.swift
+//  MiniChatTests
+//
+//  Created by Krystian Synakowski on 05/06/2026.
+//
+
+import Testing
+
+struct MiniChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
